288847df34395cc3e47e7d8694d27ed94cbae407
samoylovivan <52442440+samoylovivan@users.noreply.github.com>
2019-07-08 12:23:57 +0300

    Update and rename Main.java to dz1.java

---
+++ dz1.java
@@ -0,0 +1,20 @@
+package ivan.samoylov;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(numJewelsInStones("aAb", "caсAcA"));
+    }
+
+    public static String numJewelsInStones(String jewels, String stones) {
+        char[] arrJewels = jewels.toCharArray();
+        char[] arrStones = stones.toCharArray();
+        int countJewels = 0;
+        for (int i = 0; i < arrJewels.length; i++){
+            for(int j = 0; j < arrStones.length; j++) {
+                if(arrJewels[i] == arrStones[j]) countJewels++;
+            }
+        }
+        return Integer.toString(countJewels);
+    }
+}
